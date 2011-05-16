712431f432c897d7351431d9f834ba8ab9c6dfd4
Nathan L. Walls <nathan@wallscorp.us>
2011-05-15 22:55:42 -0400

    Fleshing out a skeleton of tests to write

--- spec/mortimer_spec.rb
+++ spec/mortimer_spec.rb
@@ -1,6 +1,25 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Mortimer do
-  it "should mortimize"
-end
+  attributes = %w(input_dir output_dir)
 
+  describe "attributes" do
+    attributes.each do |item|
+      it "should include a #{item}"
+    end
+  end
+
+  describe "validation" do
+    attributes.each do |item|
+      it "verifies #{item} is specified or raises an exception"
+    end
+
+    attributes.each do |item|
+      it "checks #{item}'s value as a valid directory"
+    end
+  end
+
+  describe "processing" do
+
+  end
+end
