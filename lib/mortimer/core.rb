a3cdae103231d33ad925b4ccb1bb81a2449f0c46
Nathan L. Walls <nathan@wallscorp.us>
2011-05-23 22:14:56 -0400

    Filling in initialize method, added a validity check

--- lib/mortimer/core.rb
+++ lib/mortimer/core.rb
@@ -2,10 +2,31 @@ module Mortimer
   class Core
     require "maruku"
 
-    attr_accessor :input_dir, :output_dir
+    attr_accessor :input_dir, :output_dir, :errors
 
-    def initialize *args
+    def initialize (input_dir, output_dir)
+      @input_dir  = input_dir
+      @output_dir = output_dir
+      @errors     = Array.new
+    end
 
+    def valid?
+      [:input_dir, :output.dir].each do |attr|
+        if self.send(attr).empty?
+          errors.push [ attr.to_s, "no value specified" ]
+        else
+          # Make sure the directory exists
+          if File.directory? self.send(attr)
+            errors.push [ attr.to_s, "Directory doesn't exist" ]
+          end
+        end
+      end
+
+      if !errors.empty?
+        return false
+      end
+
+      return true
     end
   end
 end
--- spec/mortimer/core_spec.rb
+++ spec/mortimer/core_spec.rb
@@ -1,31 +1,43 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Mortimer::Core do
+  def new_mortimer
+    @mort = Mortimer::Core.new('/tmp', '/tmp')
+  end
+
   attributes = %w(input_dir output_dir)
 
   describe "attributes" do
-    before do
-      @mort = Mortimer::Core.new
-    end
-
-    subject { @mort }
-
     attributes.each do |item|
+      before do
+        @mort = new_mortimer
+      end
+
+      subject { @mort }
       it { should respond_to item.to_sym }
     end
   end
 
   describe "validation" do
-    attributes.each do |item|
-      it "verifies #{item} is specified or raises an exception"
+    before(:each) do
+      @mort = new_mortimer
     end
 
-    attributes.each do |item|
-      it "checks #{item}'s value as a valid directory"
+    describe "verifies essential attributes" do
+      attributes.each do |item|
+        it "verifies #{item} is specified or raises an exception" do
+          pending
+        end
+      end
+    end
+
+    describe "verifies valid directory" do
+      attributes.each do |item|
+        pending
+      end
     end
   end
 
   describe "processing" do
-
   end
 end
